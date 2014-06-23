c27a47e62a3345d19d1f4fcc559b9768dff1609b
YaseGombo <ikemo77@gmail.com>
2014-06-23 15:40:49 +0900

    change the style of parameters

---
+++ rocket_dynamics_wrapper.m
@@ -0,0 +1,11 @@
+% rocket_dynamicsのラッパー
+function dx = rocket_dynamics_wrapper(t, x)
+global ROCKET;
+Ft = thrust(t, [ROCKET.Tend], [ROCKET.FT]);
+deltaY = 0;
+deltaP = 0;
+Tr = 0;
+VWH = [0; 0; 0];
+u = [Ft; deltaY; deltaP; Tr; VWH];
+dx = rocket_dynamics(t, x, u);
+end
